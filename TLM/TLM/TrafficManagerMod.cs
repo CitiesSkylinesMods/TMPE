4351a40e0f672aeb617e327f48921197e105db91
Victor-Philipp Negoescu <victor@viathinksoft.de>
2019-03-02 00:29:57 +0100

    1.10.14-alpha

--- TLM/TLM/TrafficManagerMod.cs
+++ TLM/TLM/TrafficManagerMod.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace TrafficManager {
 	public class TrafficManagerMod : IUserMod {
 
-		public static readonly string Version = "1.10.13";
+		public static readonly string Version = "1.10.14-alpha";
 
 		public static readonly uint GameVersion = 180478736u;
 		public static readonly uint GameVersionA = 1u;
