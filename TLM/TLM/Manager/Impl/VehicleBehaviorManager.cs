4d73bd6a115b49a3ca72590b80eb9c9042bbac33
Victor-Philipp Negoescu <victor@viathinksoft.de>
2019-03-02 03:14:52 +0100

    Removed unnecessary changes

--- TLM/TLM/Manager/Impl/VehicleBehaviorManager.cs
+++ TLM/TLM/Manager/Impl/VehicleBehaviorManager.cs
@@ -678,11 +678,7 @@ namespace TrafficManager.Manager.Impl {
 			return maxSpeed;
 		}
 
-		public uint GetStaticVehicleRand(ushort vehicleId) {
-			return vehicleId % 100u;
-		}
-
-		public uint GetTimedVehicleRand(ushort vehicleId) {
+		public uint GetVehicleRand(ushort vehicleId) {
 			uint intv = VehicleState.MAX_TIMED_RAND / 2u;
 			uint range = intv * (uint)(vehicleId % (100u / intv)); // is one of [0, 50]
 			uint step = VehicleStateManager.Instance.VehicleStates[vehicleId].timedRand;
@@ -695,7 +691,7 @@ namespace TrafficManager.Manager.Impl {
 
 		public float ApplyRealisticSpeeds(float speed, ushort vehicleId, ref VehicleState state, VehicleInfo vehicleInfo) {
 			if (Options.realisticSpeeds) {
-				float vehicleRand = 0.01f * (float)GetTimedVehicleRand(vehicleId);
+				float vehicleRand = 0.01f * (float)GetVehicleRand(vehicleId);
 				if (vehicleInfo.m_isLargeVehicle) {
 					speed *= 0.75f + vehicleRand * 0.25f; // a little variance, 0.75 .. 1
 				} else if (state.recklessDriver) {
@@ -1481,7 +1477,7 @@ namespace TrafficManager.Manager.Impl {
 				return false;
 			}
 
-			uint vehicleRand = GetStaticVehicleRand(vehicleId);
+			uint vehicleRand = GetVehicleRand(vehicleId);
 
 			if (vehicleRand < 100 - (int)Options.altLaneSelectionRatio) {
 #if DEBUG
