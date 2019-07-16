b64e34935ba55bacfe2420ff1c02ed5c0b1fe31b
dmytro lytovchenko <dmytro.lytovchenko@gmail.com>
2019-07-16 02:32:23 +0200

    Reformat CSUtil.Commons/*

--- TLM/CSUtil.Commons/Log.cs
+++ TLM/CSUtil.Commons/Log.cs
@@ -1,73 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Threading;
-using UnityEngine;
+﻿namespace CSUtil.Commons {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading;
+    using UnityEngine;
 
-namespace CSUtil.Commons {
+    public static class Log {
+        private static readonly object LogLock_ = new object();
 
-	public static class Log {
-		private enum LogLevel {
-			Trace,
-			Debug,
-			Info,
-			Warning,
-			Error
-		}
+        // TODO refactor log filename to configuration
+        private static readonly string LogFilename_
+            = Path.Combine(Application.dataPath, "TMPE.log");
 
-		private static object logLock = new object();
+        private enum LogLevel {
+            Trace,
+            Debug,
+            Info,
+            Warning,
+            Error
+        }
 
-		private static string logFilename = Path.Combine(Application.dataPath, "TMPE.log"); // TODO refactor log filename to configuration
-		private static Stopwatch sw = Stopwatch.StartNew();
+        private static Stopwatch sw = Stopwatch.StartNew();
 
-		static Log() {
-			try {
-				if (File.Exists(logFilename)) {
-					File.Delete(logFilename);
-				}
-			} catch (Exception) {
-				
-			}
-		}
+        static Log() {
+            try {
+                if (File.Exists(LogFilename_)) {
+                    File.Delete(LogFilename_);
+                }
+            }
+            catch (Exception) { }
+        }
 
-		[Conditional("DEBUG")]
-		public static void _Debug(string s) {
-			LogToFile(s, LogLevel.Debug);
-		}
+        [Conditional("DEBUG")]
+        public static void _Debug(string s) {
+            LogToFile(s, LogLevel.Debug);
+        }
 
-		[Conditional("TRACE")]
-		public static void _Trace(string s) {
-			LogToFile(s, LogLevel.Trace);
-		}
+        [Conditional("TRACE")]
+        public static void _Trace(string s) {
+            LogToFile(s, LogLevel.Trace);
+        }
 
-		public static void Info(string s) {
-			LogToFile(s, LogLevel.Info);
-		}
+        public static void Info(string s) {
+            LogToFile(s, LogLevel.Info);
+        }
 
-		public static void Warning(string s) {
-			LogToFile(s, LogLevel.Warning);
-		}
+        public static void Warning(string s) {
+            LogToFile(s, LogLevel.Warning);
+        }
 
-		public static void Error(string s) {
-			LogToFile(s, LogLevel.Error);
-		}
+        public static void Error(string s) {
+            LogToFile(s, LogLevel.Error);
+        }
 
-		private static void LogToFile(string log, LogLevel level) {
-			try {
-				Monitor.Enter(logLock);
-				
-				using (StreamWriter w = File.AppendText(logFilename)) {
-					w.WriteLine($"[{level.ToString()}] @ {sw.ElapsedTicks} {log}");
-					if (level == LogLevel.Warning || level == LogLevel.Error) {
-						w.WriteLine((new System.Diagnostics.StackTrace()).ToString());
-						w.WriteLine();
-					}
-				}
-			} finally {
-				Monitor.Exit(logLock);
-			}
-		}
-	}
+        private static void LogToFile(string log, LogLevel level) {
+            try {
+                Monitor.Enter(LogLock_);
 
+                using (StreamWriter w = File.AppendText(LogFilename_)) {
+                    w.WriteLine($"[{level.ToString()}] @ {sw.ElapsedTicks} {log}");
+                    if (level == LogLevel.Warning || level == LogLevel.Error) {
+                        w.WriteLine((new System.Diagnostics.StackTrace()).ToString());
+                        w.WriteLine();
+                    }
+                }
+            }
+            finally {
+                Monitor.Exit(LogLock_);
+            }
+        }
+    }
 }
--- TLM/CSUtil.Commons/VectorUtil.cs
+++ TLM/CSUtil.Commons/VectorUtil.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿namespace CSUtil.Commons {
+    using UnityEngine;
 
-namespace CSUtil.Commons {
-	public static class VectorUtil {
-		public static void ClampRectToScreen(ref Rect rect, Vector2 resolution) {
-			Log._Debug($"ClampPosToScreen([{rect.x}, {rect.y}, {rect.xMax}, {rect.yMax}], [{resolution.x}, {resolution.y}]) called");
-			if (rect.x < 0)
-				rect.x = 0;
-			if (rect.y < 0)
-				rect.y = 0;
-			if (rect.xMax >= resolution.x)
-				rect.x = resolution.x - rect.width;
-			if (rect.yMax >= resolution.y)
-				rect.y = resolution.y - rect.height;
-			Log._Debug($"ClampPosToScreen() -> [{rect.x}, {rect.y}, {rect.xMax}, {rect.yMax}]");
-		}
-	}
+    public static class VectorUtil {
+        public static void ClampRectToScreen(ref Rect rect, Vector2 resolution) {
+            Log._Debug($"ClampPosToScreen([{rect.x}, {rect.y}, {rect.xMax}, {rect.yMax}], " +
+                       $"[{resolution.x}, {resolution.y}]) called");
+            if (rect.x < 0) {
+                rect.x = 0;
+            }
+
+            if (rect.y < 0) {
+                rect.y = 0;
+            }
+
+            if (rect.xMax >= resolution.x) {
+                rect.x = resolution.x - rect.width;
+            }
+
+            if (rect.yMax >= resolution.y) {
+                rect.y = resolution.y - rect.height;
+            }
+
+            Log._Debug($"ClampPosToScreen() -> [{rect.x}, {rect.y}, {rect.xMax}, {rect.yMax}]");
+        }
+    }
 }
