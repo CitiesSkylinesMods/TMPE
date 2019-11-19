39a3af74ffe8cfa1d4cb5ec13c734c459666a0d7
kian.zarrin <kian.zarrin@gmail.com>
2019-11-19 02:11:26 +0200

    Added keybind tooltip

--- TLM/TLM/UI/MainMenu/TimedTrafficLightsButton.cs
+++ TLM/TLM/UI/MainMenu/TimedTrafficLightsButton.cs
@@ -1,4 +1,4 @@
-﻿namespace TrafficManager.UI.MainMenu {
+namespace TrafficManager.UI.MainMenu {
     using State;
 
     public class TimedTrafficLightsButton : MenuToolModeButton {
@@ -6,7 +6,7 @@
 
         protected override ButtonFunction Function => ButtonFunction.TimedTrafficLights;
 
-        public override string Tooltip => Translation.Menu.Get("Tooltip:Timed traffic lights");
+        public override string Tooltip => Translation.Menu.Get("Tooltip:Timed traffic lights") + "\n" + Translation.Menu.Get("Tooltip.Keybinds:Quick setup");
 
         public override bool Visible => Options.timedLightsEnabled;
     }
