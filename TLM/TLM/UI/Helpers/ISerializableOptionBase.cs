393f8a63f14abd1c700792eb9d4aca1e9de46df3
kian.zarrin <kian.zarrin@gmail.com>
2020-01-17 03:45:38 +0200

    pr fixes

--- TLM/TLM/UI/Helpers/ISerializableOptionBase.cs
+++ TLM/TLM/UI/Helpers/ISerializableOptionBase.cs
@@ -1,9 +1,9 @@
 namespace TrafficManager.UI.Helpers {
 
     //legacy load and save
-    interface ILegacySerializableOption
+    public interface ILegacySerializableOption
     {
-        public void Load(byte data); // TODO keep this for backward compatibality.
-        public byte Save(); // TODO: delete this once xml serialization is ready
+        void Load(byte data); // TODO keep this for backward compatibality.
+        byte Save(); // TODO: delete this once xml serialization is ready
     }
 }
