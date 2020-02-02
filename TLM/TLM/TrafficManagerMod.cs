57894939e0745a9527b214f6f869963609a5a876
Aubergine <guy.fraser1@gmail.com>
2020-02-02 22:54:10 +0000

    Update TrafficManagerMod.cs
    Updated version numbers and other tweaks to bring inline with master branch.
    
    Not sure what to put for the `public const uint GAME_VERSION = 184803856u;` value.

--- TLM/TLM/TrafficManagerMod.cs
+++ TLM/TLM/TrafficManagerMod.cs
@@ -1,69 +1,107 @@
-using CSUtil.Commons;
-using ICities;
-using System.Reflection;
-using ColossalFramework.UI;
-using TrafficManager.State;
-using TrafficManager.Util;
-
 namespace TrafficManager
 {
+    using ColossalFramework.Globalization;
+    using ColossalFramework.UI;
+    using CSUtil.Commons;
+    using ICities;
+    using JetBrains.Annotations;
+    using System.Reflection;
+    using System;
+    using TrafficManager.State;
+    using TrafficManager.UI;
+    using TrafficManager.Util;
+
     public class TrafficManagerMod : IUserMod
     {
-        public static readonly uint GameVersion = 184803856u;
-        public static readonly uint GameVersionA = 1u;
-        public static readonly uint GameVersionB = 12u;
-        public static readonly uint GameVersionC = 1u;
-        public static readonly uint GameVersionBuild = 2u;
-
-        // Note: `Version` is also used in UI/MainMenu/VersionLabel.cs
-        public static readonly string Version = "11.0";
-
 #if LABS
-        public string Branch => "LABS";
+        public const string BRANCH = "LABS";
 #elif DEBUG
-        public string Branch => "DEBUG";
+        public const string BRANCH = "DEBUG";
 #else
-        public string Branch => "STABLE";
+        public const string BRANCH = "STABLE";
 #endif
 
-        public string Name => "TM:PE " + Version + " " + Branch;
+        public const uint GAME_VERSION = 184803856u;
+        public const uint GAME_VERSION_A = 1u;
+        public const uint GAME_VERSION_B = 12u;
+        public const uint GAME_VERSION_C = 3u;
+        public const uint GAME_VERSION_BUILD = 2u;
+
+        public const string VERSION = "11.0";
+
+        public static readonly string ModName = "TM:PE " + BRANCH + " " + VERSION;
+
+        public string Name => ModName;
 
         public string Description => "Manage your city's traffic";
 
+        [UsedImplicitly]
         public void OnEnabled()
         {
-            Log.Info($"TM:PE enabled. Version {Version}, Build {Assembly.GetExecutingAssembly().GetName().Version} {Branch} for game version {GameVersionA}.{GameVersionB}.{GameVersionC}-f{GameVersionBuild}");
-            Log.Info($"Enabled TM:PE has GUID {Assembly.GetExecutingAssembly().ManifestModule.ModuleVersionId}");
+            Log.InfoFormat(
+                "TM:PE enabled. Version {0}, Build {1} {2} for game version {3}.{4}.{5}-f{6}",
+                VERSION,
+                Assembly.GetExecutingAssembly().GetName().Version,
+                BRANCH,
+                GAME_VERSION_A,
+                GAME_VERSION_B,
+                GAME_VERSION_C,
+                GAME_VERSION_BUILD);
+            Log.InfoFormat(
+                "Enabled TM:PE has GUID {0}",
+                Assembly.GetExecutingAssembly().ManifestModule.ModuleVersionId);
 
             // check for incompatible mods
             if (UIView.GetAView() != null)
-            { // when TM:PE is enabled in content manager
+            {
+                // when TM:PE is enabled in content manager
                 CheckForIncompatibleMods();
             }
             else
-            { // or when game first loads if TM:PE was already enabled
+            {
+                // or when game first loads if TM:PE was already enabled
                 LoadingManager.instance.m_introLoaded += CheckForIncompatibleMods;
+            }
+
+            // Log Mono version
+            Type monoRt = Type.GetType("Mono.Runtime");
+            if (monoRt != null)
+            {
+                MethodInfo displayName = monoRt.GetMethod(
+                    "GetDisplayName",
+                    BindingFlags.NonPublic | BindingFlags.Static);
+                if (displayName != null)
+                {
+                    Log.InfoFormat("Mono version: {0}", displayName.Invoke(null, null));
+                }
             }
         }
 
+        [UsedImplicitly]
         public void OnDisabled()
         {
             Log.Info("TM:PE disabled.");
             LoadingManager.instance.m_introLoaded -= CheckForIncompatibleMods;
+            LocaleManager.eventLocaleChanged -= Translation.HandleGameLocaleChange;
+            Translation.IsListeningToGameLocaleChanged = false; // is this necessary?
         }
 
+        [UsedImplicitly]
         public void OnSettingsUI(UIHelperBase helper)
         {
+            // Note: This bugs out if done in OnEnabled(), hence doing it here instead.
+            if (!Translation.IsListeningToGameLocaleChanged)
+            {
+                Translation.IsListeningToGameLocaleChanged = true;
+                LocaleManager.eventLocaleChanged += new LocaleManager.LocaleChangedHandler(Translation.HandleGameLocaleChange);
+            }
             Options.MakeSettings(helper);
         }
 
         private static void CheckForIncompatibleMods()
         {
-            if (GlobalConfig.Instance.Main.ScanForKnownIncompatibleModsAtStartup)
-            {
-                ModsCompatibilityChecker mcc = new ModsCompatibilityChecker();
-                mcc.PerformModCheck();
-            }
+            ModsCompatibilityChecker mcc = new ModsCompatibilityChecker();
+            mcc.PerformModCheck();
         }
     }
 }
