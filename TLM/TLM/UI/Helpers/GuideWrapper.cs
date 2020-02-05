f4aec1c77735558f66094359dd187103992a46fb
kian.zarrin <kian.zarrin@gmail.com>
2020-02-05 09:05:24 +0200

    remved test key from Guides.csv used default guide delay values

--- TLM/TLM/UI/Helpers/GuideWrapper.cs
+++ TLM/TLM/UI/Helpers/GuideWrapper.cs
@@ -9,9 +9,10 @@ namespace TrafficManager.UI {
         public GuideWrapper(string key) {
             m_guide = new GenericGuide();
             m_info = new GuideInfo {
+                // These values are game defaults:
                 m_delayType = GuideInfo.Delay.OccurrenceCount,
                 m_displayDelay = 1,
-                m_repeatDelay = 1, //3
+                m_repeatDelay = 3, 
                 m_overrideOptions = true,
                 m_icon = "ToolbarIconZoomOutGlobe",
                 m_tag = "Generic",
