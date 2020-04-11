93970f8d22321f028dff8326476add2d63576851
dmytro lytovchenko <dmytro.lytovchenko@gmail.com>
2020-04-11 12:48:46 +0200

    TimedTrafficLights states moved inside TTL tool; Fixes #826

---
+++ TLM/TLM/UI/SubTools/TimedTrafficLights/TTLToolMode.cs
@@ -0,0 +1,18 @@
+namespace TrafficManager.UI.SubTools {
+    internal enum TTLToolMode {
+        /// <summary>Timed traffic light submode.</summary>
+        SelectNode,
+
+        /// <summary>Timed traffic light submode.</summary>
+        ShowLights,
+
+        /// <summary>Timed traffic light submode.</summary>
+        AddNode,
+
+        /// <summary>Timed traffic light submode.</summary>
+        RemoveNode,
+
+        /// <summary>Timed traffic light submode.</summary>
+        CopyLights,
+    }
+}
