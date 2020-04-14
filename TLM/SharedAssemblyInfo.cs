7e5c330b609c64f005d2bf86693f309765bc254d
aubergine10 <guy.fraser1@gmail.com>
2020-04-14 18:09:26 +0100

    Bump version and update changelogs

--- TLM/SharedAssemblyInfo.cs
+++ TLM/SharedAssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("11.3.1.*")]
+[assembly: AssemblyVersion("11.3.2.*")]
