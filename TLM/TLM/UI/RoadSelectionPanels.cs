bceb370336c9bc89bc6afe63414cf4a6a2eedcc1
kian.zarrin <kian.zarrin@gmail.com>
2020-05-14 18:14:15 +0300

    minor code fix

--- TLM/TLM/UI/RoadSelectionPanels.cs
+++ TLM/TLM/UI/RoadSelectionPanels.cs
@@ -348,8 +348,8 @@ namespace TrafficManager.UI {
             }
 
             public class ClearButtton : ButtonExt {
-                public virtual string SkinPrefix => Function.ToString(); // remove _RHT/_LHT postFix.
                 protected override string GetTooltip() => Translation.Menu.Get("RoadSelection.Tooltip:Clear");
+                public override string SkinPrefix => Function.ToString(); // remove _RHT/_LHT postFix.
                 internal override FunctionModes Function => FunctionModes.Clear;
                 protected override bool IsActive() => false; // Clear funtionality can't be undone. #568
                 public override void Do() => // TODO delete all rules as part of #568
