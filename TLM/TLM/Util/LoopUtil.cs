973145a56e8a354830c9241cda7f04fa76a9282e
egi <daegi01@gmail.com>
2020-10-26 01:58:36 +0100

    Deprecated SpiralLoop cleanup.
    Removed the SpiralLoop method.
    Adjusted unit- and performance tests.
    performance tests should use a mono path that should be more common for CSL.

--- TLM/TLM/Util/LoopUtil.cs
+++ TLM/TLM/Util/LoopUtil.cs
@@ -1,50 +1,8 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace TrafficManager.Util {
     public static class LoopUtil {
-        public delegate bool TwoDimLoopHandler(int x, int y);
-
-        [Obsolete("Use 'GenerateSpiralGridCoordsClockwise' instead.")]
-        public static void SpiralLoop(int xCenter,
-                                      int yCenter,
-                                      int width,
-                                      int height,
-                                      TwoDimLoopHandler handler) {
-            SpiralLoop(
-                width,
-                height,
-                (x, y) => handler(xCenter + x, yCenter + y));
-        }
-
-        [Obsolete("Use 'GenerateSpiralGridCoordsClockwise' instead.")]
-        public static void SpiralLoop(int width, int height, TwoDimLoopHandler handler) {
-            int x, y, dx, dy;
-            x = y = dx = 0;
-            dy = -1;
-            int t = width > height ? width : height;
-            int maxI = t * t;
-            for (int i = 0; i < maxI; i++) {
-                if ((-width / 2 <= x) && (x <= width / 2) && (-height / 2 <= y)
-                    && (y <= height / 2))
-                {
-                    if (!handler(x, y)) {
-                        break;
-                    }
-                }
-
-                if ((x == y) || ((x < 0) && (x == -y)) || ((x > 0) && (x == 1 - y))) {
-                    t = dx;
-                    dx = -dy;
-                    dy = t;
-                }
-
-                x += dx;
-                y += dy;
-            }
-        }
-
         /// <summary>
         /// Generates a stream of spiral grid coordinates clockwise.
         /// </summary>
