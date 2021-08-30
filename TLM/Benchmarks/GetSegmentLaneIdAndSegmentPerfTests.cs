2d0d7fd9a791bbc65c89fed1405580dfa660a43e
egi <daegi01@gmail.com>
2021-08-30 19:59:15 +0200

    Added perf tests to ensure that there is no heap allocation during direct foreach and as a base line for further improvement.

---
+++ TLM/Benchmarks/GetSegmentLaneIdAndSegmentPerfTests.cs
@@ -0,0 +1,33 @@
+using BenchmarkDotNet.Attributes;
+using GenericGameBridge.Service;
+using System;
+using System.Collections.Generic;
+
+namespace Benchmarks {
+    public class GetSegmentLaneIdAndSegmentPerfTests {
+        private readonly static NetLane[] testLanes = new NetLane[] {
+            new NetLane() {
+                m_nextLane = 1
+            },
+            new NetLane() {
+                m_nextLane = 2
+            },
+            new NetLane() {
+                m_nextLane = 0
+            },
+        };
+
+        [Benchmark]
+        public void Foreach_Direct() {
+            foreach (var item in new GetSegmentLaneIdsEnumerable(0, testLanes.Length, testLanes)) {
+            }
+        }
+
+        [Benchmark]
+        public void Foreach_ViaInterface() {
+            IEnumerable<LaneIdAndLaneIndex> nodeSegmentIds = new GetSegmentLaneIdsEnumerable(0, testLanes.Length, testLanes);
+            foreach (var item in nodeSegmentIds) {
+            }
+        }
+    }
+}
