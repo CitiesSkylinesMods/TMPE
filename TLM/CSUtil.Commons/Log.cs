fdbe4218eff7f2fda52ed97bc072ceb0a6f8b598
kianzzarrin2 <kian.zarrin@gmail.com>
2021-09-03 01:50:46 +0300

    configurable log file path.

--- TLM/CSUtil.Commons/Log.cs
+++ TLM/CSUtil.Commons/Log.cs
@@ -5,11 +5,14 @@ namespace CSUtil.Commons {
     using System.Threading;
     using UnityEngine;
     using Debug = UnityEngine.Debug;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
 
 #if !DEBUG
-    #if TRACE
-        #error TRACE is defined outside of a DEBUG build, please remove
-    #endif
+#if TRACE
+#error TRACE is defined outside of a DEBUG build, please remove
+#endif
 #endif
 
     /// <summary>
@@ -40,9 +43,7 @@ namespace CSUtil.Commons {
     public static class Log {
         private static readonly object LogLock = new object();
 
-        // TODO refactor log filename to configuration
-        private static readonly string LogFilename
-            = Path.Combine(Application.dataPath, "TMPE.log");
+        private static readonly string LogFilePath;
 
         private enum LogLevel {
             Trace,
@@ -56,8 +57,22 @@ namespace CSUtil.Commons {
 
         static Log() {
             try {
-                if (File.Exists(LogFilename)) {
-                    File.Delete(LogFilename);
+                string logFileName = "TMPE.log";
+                string dir = Application.dataPath;
+                LogFilePath = Path.Combine(dir, logFileName);
+                if (File.Exists(LogFilePath)) {
+                    File.Delete(LogFilePath); // delete old file to avoid confusion.
+                }
+
+                var args = Environment.GetCommandLineArgs().ToList();
+                int index = args.IndexOf("-logFile");
+                if (index >= 0) {
+                    dir = args[index + 1];
+                    dir = Path.GetDirectoryName(dir); // drop output_log.txt
+                    LogFilePath = Path.Combine(dir, logFileName);
+                    if (File.Exists(LogFilePath)) {
+                        File.Delete(LogFilePath);
+                    }
                 }
             }
             catch (Exception e) {
@@ -196,7 +211,7 @@ namespace CSUtil.Commons {
 
         private static void LogToFile(string log, LogLevel level) {
             lock(LogLock){
-                using (StreamWriter w = File.AppendText(LogFilename)) {
+                using (StreamWriter w = File.AppendText(LogFilePath)) {
                     long secs = _sw.ElapsedTicks / Stopwatch.Frequency;
                     long fraction = _sw.ElapsedTicks % Stopwatch.Frequency;
                     w.WriteLine(
