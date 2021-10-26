d6f957fe79f69182332bc4123f6b2c85a4282a32
egi <daegi01@gmail.com>
2021-10-26 21:29:05 +0200

    Moved IsVehicleValid method to ExtVehicleManager.

--- TLM/TMPE.CitiesGameBridge/Service/VehicleService.cs
+++ TLM/TMPE.CitiesGameBridge/Service/VehicleService.cs
@@ -1,6 +1,4 @@
 namespace CitiesGameBridge.Service {
-    using ColossalFramework;
-    using CSUtil.Commons;
     using GenericGameBridge.Service;
 
     public class VehicleService : IVehicleService {
@@ -9,52 +7,5 @@ namespace CitiesGameBridge.Service {
         private VehicleService() { }
 
         public int MaxVehicleCount => VehicleManager.instance.m_vehicles.m_buffer.Length;
-
-        /// <summary>
-        /// Check vehicle flags contain at least one of the flags in <paramref name="flagMask"/>.
-        /// </summary>
-        /// 
-        /// <param name="vehicleId">The id of the vehicle to inspect.</param>
-        /// <param name="flagMask">The flags to test.</param>
-        /// <param name="expectedResult">If specified, ensure only the expected flags are found.</param>
-        /// 
-        /// <returns>Returns <c>true</c> if the test passes, otherwise <c>false</c>.</returns>
-        public bool CheckVehicleFlags(ushort vehicleId,
-                                      Vehicle.Flags flagMask,
-                                      Vehicle.Flags? expectedResult = null) {
-
-            Vehicle.Flags result =
-                Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags
-                & flagMask;
-
-            return expectedResult == null ? result != 0 : result == expectedResult;
-        }
-
-        /// <summary>
-        /// Check vehicle flags2 contain at least one of the flags in <paramref name="flagMask"/>.
-        /// </summary>
-        /// 
-        /// <param name="vehicleId">The id of the vehicle to inspect.</param>
-        /// <param name="flagMask">The flags to test.</param>
-        /// <param name="expectedResult">If specified, ensure only the expected flags are found.</param>
-        /// 
-        /// <returns>Returns <c>true</c> if the test passes, otherwise <c>false</c>.</returns>
-        public bool CheckVehicleFlags2(ushort vehicleId,
-                                       Vehicle.Flags2 flagMask,
-                                       Vehicle.Flags2? expectedResult = null) {
-
-            Vehicle.Flags2 result =
-                Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleId].m_flags2
-                & flagMask;
-
-            return expectedResult == null ? result != 0 : result == expectedResult;
-        }
-
-        public bool IsVehicleValid(ushort vehicleId) {
-            return CheckVehicleFlags(
-                vehicleId,
-                Vehicle.Flags.Created | Vehicle.Flags.Deleted,
-                Vehicle.Flags.Created);
-        }
     }
 }
--- TLM/TMPE.GenericGameBridge/Service/IVehicleService.cs
+++ TLM/TMPE.GenericGameBridge/Service/IVehicleService.cs
@@ -1,15 +1,5 @@
 namespace GenericGameBridge.Service {
     public interface IVehicleService {
         int MaxVehicleCount { get; }
-
-        bool CheckVehicleFlags(ushort vehicleId,
-                               Vehicle.Flags flagMask,
-                               Vehicle.Flags? expectedResult = default(Vehicle.Flags?));
-
-        bool CheckVehicleFlags2(ushort vehicleId,
-                                Vehicle.Flags2 flagMask,
-                                Vehicle.Flags2? expectedResult = default(Vehicle.Flags2?));
-
-        bool IsVehicleValid(ushort vehicleId);
     }
 }
