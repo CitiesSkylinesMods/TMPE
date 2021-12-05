1ff7728eaec679f4c21114378b0a2c654ad77c30
kianzzarrin <kian.zarrin@gmail.com>
2021-12-05 10:03:35 +0200

    fixed JRM transparency

--- TLM/TLM/UI/Helpers/TrafficRulesOverlay.cs
+++ TLM/TLM/UI/Helpers/TrafficRulesOverlay.cs
@@ -83,7 +83,7 @@ namespace TrafficManager.UI.Helpers {
 
                 dirX_ = (segmentEnd.LeftCorner - segmentEnd.RightCorner).normalized;
 
-                // for curved angled segements, corner1Direction may slightly differ from corner2Direction
+                // for curved angled segments, corner1Direction may slightly differ from corner2Direction
                 dirY_ = (segmentEnd.LeftCornerDir + segmentEnd.RightCornerDir) * 0.5f;
 
                 // origin point to start drawing sprites from.
@@ -138,11 +138,7 @@ namespace TrafficManager.UI.Helpers {
                 if (viewOnly_) {
                     // Readonly signs look grey-ish
                     guiColor = Color.Lerp(guiColor, Color.gray, 0.5f);
-                    guiColor.a = TrafficManagerTool.GetHandleAlpha(hovered: false);
                 } else {
-                    // Handles in edit mode are always visible. Hovered handles are also highlighted.
-                    guiColor.a = 1f;
-
                     if (hoveredHandle) {
                         guiColor = Color.Lerp(
                             a: guiColor,
@@ -150,7 +146,7 @@ namespace TrafficManager.UI.Helpers {
                             t: 0.5f);
                     }
                 }
-                // guiColor.a = TrafficManagerTool.GetHandleAlpha(hoveredHandle);
+                guiColor.a = TrafficManagerTool.GetHandleAlpha(hoveredHandle);
 
                 GUI.color = guiColor;
                 GUI.DrawTexture(boundingBox, signTexture);
