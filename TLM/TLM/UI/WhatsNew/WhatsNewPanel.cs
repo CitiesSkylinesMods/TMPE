d89f81b17c7536570153ed21c93f45df4ddb4612
aubergine10 <guy.fraser1@gmail.com>
2022-01-18 18:32:24 +0000

    Minor change to panel title
    s/in/-

--- TLM/TLM/UI/WhatsNew/WhatsNewPanel.cs
+++ TLM/TLM/UI/WhatsNew/WhatsNewPanel.cs
@@ -215,7 +215,7 @@ namespace TrafficManager.UI.WhatsNew {
             title.anchor = UIAnchorStyle.Top;
             title.textAlignment = UIHorizontalAlignment.Center;
             title.eventTextChanged += (_, _) => title.CenterToParent();
-            title.text = "What's New in " + TrafficManagerMod.ModName;
+            title.text = "What's New - " + TrafficManagerMod.ModName;
             title.MakePixelPerfect();
 
             var cancel = _header.AddUIComponent<UIButton>();
