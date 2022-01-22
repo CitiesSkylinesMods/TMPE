fc6301aa7d5f18e12ddcbb3ad53514a9e9db0166
aubergine10 <guy.fraser1@gmail.com>
2022-01-22 04:18:50 +0000

    Extended `CheckboxOption` from #1301
    - If any problems with this, please review in #1301

--- TLM/TLM/UI/Helpers/CheckboxOption.cs
+++ TLM/TLM/UI/Helpers/CheckboxOption.cs
@@ -4,7 +4,8 @@ namespace TrafficManager.UI.Helpers {
     using State;
 
     public class CheckboxOption : SerializableUIOptionBase<bool, UICheckBox> {
-        public CheckboxOption(string fieldName) : base(fieldName) {
+        public CheckboxOption(string fieldName, bool isGlobalOption = false)
+        : base(fieldName, isGlobalOption) {
             OnValueChanged = DefaultOnValueChanged;
         }
 
@@ -24,22 +25,40 @@ namespace TrafficManager.UI.Helpers {
             }
         }
 
-        public override void Load(byte data) => Value = (data != 0);
+        public override bool Enabled {
+            get => base.Enabled;
+            set {
+                base.Enabled = value;
+                if (_ui != null) {
+                    _ui.readOnly = !value;
+                    _ui.opacity = value ? 1f : 0.3f;
+                }
+            }
+        }
+
+        public override void Load(byte data) => Value = data != 0;
         public override byte Save() => Value ? (byte)1 : (byte)0;
 
         public override void AddUI(UIHelperBase container) {
-            string T(string key) => Translation.Options.Get(key);
+            bool unalterable = !GlobalOption && !Options.IsGameLoaded(false);
             _ui = container.AddCheckbox(
                 T(Label),
                 Value,
                 this.OnValueChanged) as UICheckBox;
-            if (Tooltip != null) {
-                _ui.tooltip = T(Tooltip);
-            }
             if (Indent) {
-                State.Options.Indent(_ui);
+                Options.Indent(_ui);
             }
             Options.AllowTextWrap(_ui, Indent);
+            if (unalterable) {
+                Enabled = false;
+                _ui.tooltip = "This setting can only be changed in-game.";
+            } else if (Tooltip != null) {
+                _ui.tooltip = T(Tooltip);
+                if (!base.Enabled) {
+                    Enabled = false;
+                }
+            }
         }
+
     }
 }
--- TLM/TLM/UI/Helpers/SerializableUIOptionBase.cs
+++ TLM/TLM/UI/Helpers/SerializableUIOptionBase.cs
@@ -10,13 +10,19 @@ namespace TrafficManager.UI.Helpers {
     public abstract class SerializableUIOptionBase<TVal, TUI> : ILegacySerializableOption
         where TUI : UIComponent {
         //Data:
-        public SerializableUIOptionBase(string fieldName) {
+        public SerializableUIOptionBase(string fieldName, bool globalOption) {
+           
             ValueField = typeof(Options).GetField(fieldName, BindingFlags.Static | BindingFlags.Public);
             if (ValueField == null) {
-                throw new Exception($"{typeof(Options)}.{fieldName} does not exists");
+                throw new Exception($"{typeof(Options)}.{fieldName} does not exist");
             }
+
+            GlobalOption = globalOption;
         }
         private FieldInfo ValueField;
+        public bool GlobalOption { get; private set; }
+        public string Translator { get; set; }
+
         private Options OptionInstance => Singleton<Options>.instance;
         public virtual TVal Value {
             get => (TVal)ValueField.GetValue(null);
@@ -27,16 +33,32 @@ namespace TrafficManager.UI.Helpers {
         public abstract byte Save();
 
         //UI:
-        public abstract void AddUI(UIHelperBase container);
-        protected TUI _ui;
         public string Label;
         public string Tooltip;
         public bool Indent = false;
+        public abstract void AddUI(UIHelperBase container);
+        protected TUI _ui;
+
+        public virtual bool Enabled { get; set; }
 
         public void DefaultOnValueChanged(TVal newVal) {
-            Options.IsGameLoaded();
+            Options.IsGameLoaded(!GlobalOption);
             Log._Debug($"{Label} changed to {newVal}");
             Value = newVal;
         }
+
+        protected string T(string key) {
+            if (string.IsNullOrEmpty(Translator)) {
+                return Translation.Options.Get(key);
+            }
+            PropertyInfo prop = typeof(Translation)
+                .GetProperty(Translator, BindingFlags.Static | BindingFlags.Public);
+            if (prop == null) {
+                throw new Exception($"{typeof(Translation)}.{Translator} does not exist");
+            }
+            Localization.LookupTable translator = (Localization.LookupTable)prop.GetValue(typeof(Translation), null);
+            return translator.Get(key);
+        }
+
     }
 }
