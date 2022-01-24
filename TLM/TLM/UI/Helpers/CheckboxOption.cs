7c3f7e8ff3678efccf9658af895884aa23da5145
aubergine10 <guy.fraser1@gmail.com>
2022-01-24 18:06:18 +0000

    Refactor CheckboxOption.cs
    - Use new member names from base class
    - Fix bug in `AddUI`
    - Handle UI updates in getters (where applicable)
    - Simplify references

--- TLM/TLM/UI/Helpers/CheckboxOption.cs
+++ TLM/TLM/UI/Helpers/CheckboxOption.cs
@@ -9,54 +9,80 @@ namespace TrafficManager.UI.Helpers {
             OnValueChanged = DefaultOnValueChanged;
         }
 
-        public event OnCheckChanged OnValueChanged;
+        /* Data */
 
+        public event OnCheckChanged OnValueChanged;
         public OnCheckChanged Handler {
             set => OnValueChanged += value;
+        }
+        public override void Load(byte data) => Value = data != 0;
+        public override byte Save() => Value ? (byte)1 : (byte)0;
+
+        /* UI */
+
+        public string Label {
+            get => _label ?? FieldName;
+            set {
+                _label = value;
+                if (HasUI) {
+                    _ui.label.text = string.IsNullOrEmpty(value)
+                        ? string.Empty // avoid invalidating UI if already no label
+                        : T(value);
+                }
+            }
+        }
+
+        public string Tooltip {
+            get => _tooltip;
+            set {
+                _tooltip = value;
+                if (HasUI) {
+                    _ui.tooltip = IsInScope
+                        ? string.IsNullOrEmpty(value)
+                            ? string.Empty // avoid invalidating UI if already no tooltip
+                            : T(value)
+                        : T(INGAME_ONLY_SETTING);
+                }
+            }
         }
 
         public override bool Value {
             get => base.Value;
             set {
                 base.Value = value;
-                if (_ui != null) {
+                if (HasUI) {
                     _ui.isChecked = value;
                 }
             }
         }
 
-        public override bool ReadOnlyUI {
-            get => base.ReadOnlyUI;
+        public bool ReadOnlyUI {
+            get => _readOnlyUI;
             set {
-                base.ReadOnlyUI = !IsInScope || value;
-                if (_ui != null) {
-                    _ui.readOnly = base.ReadOnlyUI;
-                    _ui.opacity = base.ReadOnlyUI ? 0.3f : 1f;
+                _readOnlyUI = !IsInScope || value;
+                if (HasUI) {
+                    _ui.readOnly = _readOnlyUI;
+                    _ui.opacity = _readOnlyUI ? 0.3f : 1f;
                 }
             }
         }
 
-        public override void Load(byte data) => Value = data != 0;
-        public override byte Save() => Value ? (byte)1 : (byte)0;
-
-        public override void AddUI(UIHelperBase container) {
-            _ui = container.AddCheckbox(
-                T(Label),
-                Value,
-                this.OnValueChanged) as UICheckBox;
-            if (Indent) {
-                Options.Indent(_ui);
-            }
-            Options.AllowTextWrap(_ui, Indent);
-            if (!IsInScope) {
-                ReadOnlyUI = true;
-                _ui.tooltip = "This setting can only be changed in-game.";
-            } else if (Tooltip != null) {
-                _ui.tooltip = T(Tooltip);
-                if (base.ReadOnlyUI) {
-                    ReadOnlyUI = true;
+        public bool Indent {
+            get => _indent;
+            set {
+                _indent = value;
+                if (HasUI && _indent) {
+                    Options.Indent(_ui);
                 }
             }
+        }
+
+        public override void AddUI(UIHelperBase container) {
+            _ui = container.AddCheckbox(T(Label), Value, OnValueChanged) as UICheckBox;
+            Indent = _indent;
+            Options.AllowTextWrap(_ui, Indent);
+            Tooltip = _tooltip;
+            ReadOnlyUI = _readOnlyUI;
         }
 
     }
