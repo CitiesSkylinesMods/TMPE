de5c6cf71c3834ab20f348d023256b4d582152ce
krzychu124 <krzychu124@gmail.com>
2022-01-25 10:09:16 +0100

    Compatibility update

--- TLM/TLM/Patch/_CitizenAI/_TouristAI/Connection/TouristAIConnection.cs
+++ TLM/TLM/Patch/_CitizenAI/_TouristAI/Connection/TouristAIConnection.cs
@@ -1,9 +1,10 @@
 namespace TrafficManager.Patch._CitizenAI._TouristAI.Connection {
     using System;
+    using UnityEngine;
 
     public delegate int GetTaxiProbabilityDelegate(TouristAI instance);
     public delegate int GetBikeProbabilityDelegate(TouristAI instance);
-    public delegate int GetCarProbabilityDelegate(TouristAI instance);
+    public delegate int GetCarProbabilityDelegate(TouristAI instance, Vector3 position);
     public delegate int GetElectricCarProbabilityDelegate(TouristAI instance, Citizen.Wealth wealth);
     public delegate int GetCamperProbabilityDelegate(TouristAI instance, Citizen.Wealth wealth);
 
