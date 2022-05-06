918ea2d93006d73aa59a3cbb36f80060095699e9
Elesbaan70 <95266227+Elesbaan70@users.noreply.github.com>
2022-05-06 00:22:46 -0500

    Fill in missing pieces

--- TLM/TMPE.API/Hook/IJunctionRestrictionsHook.cs
+++ TLM/TMPE.API/Hook/IJunctionRestrictionsHook.cs
@@ -7,22 +7,53 @@ using TrafficManager.API.Traffic.Enums;
 namespace TrafficManager.API.Hook {
     public interface IJunctionRestrictionsHook {
 
-        event FlagsHookHandler GetDefaults;
+        /// <summary>
+        /// An event that allows a handler to modify the results of a <c>GetDefault<i>TrafficRule</i></c> method.
+        /// </summary>
+        event Action<FlagsHookArgs> GetDefaults;
 
-        event FlagsHookHandler GetConfigurable;
+        /// <summary>
+        /// An event that allows a handler to modify the results of an <c>Is<i>TrafficRule</i>Configurable</c> method.
+        /// </summary>
+        event Action<FlagsHookArgs> GetConfigurable;
+
+        /// <summary>
+        /// Invalidates the specified flags so that they will be recalculated.
+        /// Recalculation is not guaranteed to happen immediately, but is guaranteed to happen
+        /// before their next use.
+        /// </summary>
+        /// <param name="flags"></param>
+        public void InvalidateFlags(JunctionRestrictionFlags flags);
 
         public class FlagsHookArgs {
 
+            /// <summary>
+            /// Identifies the segment for which flag data is being returned.
+            /// </summary>
+            public ushort SegmentId { get; private set; }
+
+            /// <summary>
+            /// Identifies the node on the segment for which flag data is being returned.
+            /// </summary>
+            public bool StartNode { get; private set; }
+
+            /// <summary>
+            /// Identifies which flags are being returned. Unnecessary computation may be avoided
+            /// by examining this mask to see which flags are being requested.
+            /// </summary>
             public JunctionRestrictionFlags Mask { get; private set; }
 
+            /// <summary>
+            /// The flag return values. Changes to this property alter the outcome of the underlying operation.
+            /// </summary>
             public JunctionRestrictionFlags Result { get; set; }
 
-            public FlagsHookArgs(JunctionRestrictionFlags mask, JunctionRestrictionFlags result) {
+            internal FlagsHookArgs(ushort segmentId, bool startNode, JunctionRestrictionFlags mask, JunctionRestrictionFlags result) {
+                SegmentId = segmentId;
+                StartNode = startNode;
                 Mask = mask;
                 Result = result;
             }
         }
-
-        public delegate void FlagsHookHandler(FlagsHookArgs args);
     }
 }
