6f162fef7b994a343c1e8226c2590fd8d55f2532
aubergine10 <guy.fraser1@gmail.com>
2022-05-09 06:50:24 +0100

    Remove unnecessary usings

--- TLM/TLM/State/OptionsTabs/GeneralTab.cs
+++ TLM/TLM/State/OptionsTabs/GeneralTab.cs
@@ -1,10 +1,5 @@
 namespace TrafficManager.State {
-    using UnityEngine;
-    using TrafficManager.Lifecycle;
-    using TrafficManager.Manager.Impl;
-    using TrafficManager.State.ConfigData;
     using TrafficManager.UI;
-    using TrafficManager.UI.Textures;
     using TrafficManager.UI.Helpers;
     using TrafficManager.UI.WhatsNew;
 
