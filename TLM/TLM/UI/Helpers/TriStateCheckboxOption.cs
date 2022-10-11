d36c4ec68c7697d310a58fa06287507257211da6
kianzzarrin <kian.zarrin@gmail.com>
2022-10-11 20:21:18 +0300

    tristate checkbox option

---
+++ TLM/TLM/UI/Helpers/TriStateCheckboxOption.cs
@@ -0,0 +1,147 @@
+namespace TrafficManager.UI.Helpers {
+    using ICities;
+    using ColossalFramework.UI;
+    using TrafficManager.State;
+    using CSUtil.Commons;
+    using UnityEngine;
+    using TrafficManager.Util.Extensions;
+    using JetBrains.Annotations;
+    using System.Collections.Generic;
+
+    public class TriStateCheckboxOption :
+        SerializableUIOptionBase<TernaryBool, UITriStateCheckbox, TriStateCheckboxOption>, IValuePropagator {
+        private const int LABEL_MAX_WIDTH = 615;
+        private const int LABEL_MAX_WIDTH_INDENTED = 600;
+
+        public TriStateCheckboxOption(string fieldName, Options.PersistTo scope = Options.PersistTo.Savegame)
+        : base(fieldName, scope) { }
+
+        private HashSet<IValuePropagator> _propagatesTrueTo = new();
+        private HashSet<IValuePropagator> _propagatesFalseTo = new();
+
+        public TriStateCheckboxOption PropagateTrueTo([NotNull] IValuePropagator target) {
+            Log.Info($"TriStateCheckboxOption.PropagateTrueTo: `{FieldName}` will propagate to `{target}`");
+            this.AddPropagate(target, true);
+            target.AddPropagate(this, false);
+            return this;
+        }
+
+        private HashSet<IValuePropagator> GetTargetPropagates(bool value) =>
+            value ? _propagatesTrueTo : _propagatesFalseTo;
+
+        public void AddPropagate(IValuePropagator target, bool value) =>
+            GetTargetPropagates(value).Add(target);
+
+        public void Propagate(bool value) {
+            if (!value)
+                Value2 = null;
+        }
+
+        private void PropagateAll(bool value) {
+            foreach (var target in GetTargetPropagates(value))
+                target.Propagate(value);
+        }
+
+        public override string ToString() => FieldName;
+
+        public override void Load(byte data) => Value = (TernaryBool)data;
+
+        public override byte Save() => (byte)Value;
+
+        public override TernaryBool Value {
+            get => base.Value;
+            set {
+                if (value != base.Value) {
+                    base.Value = value;
+                    Log.Info($"TriStateCheckboxOption.Value: `{FieldName}` changed to {value}");
+                    PropagateAll(Value2.HasValue);
+                    if (HasUI) _ui.Value = Value2;
+                }
+            }
+        }
+
+        public bool? Value2 {
+            get => TernaryBoolUtil.ToOptBool(Value);
+            set => Value = TernaryBoolUtil.ToTernaryBool(value);
+        }
+
+        public static implicit operator bool?(TriStateCheckboxOption a) => a.Value2;
+
+        private void HandleValueChanged(UIComponent c, bool? value) =>
+            InvokeOnValueChanged(TernaryBoolUtil.ToTernaryBool(value));
+
+        public override TriStateCheckboxOption AddUI(UIHelperBase container) {
+            _ui = container.AddUIComponent<UITriStateCheckbox>();
+            _ui.eventValueChanged += HandleValueChanged;
+            if (Indent) ApplyIndent(_ui);
+            UpdateLabel();
+            ApplyTextWrap(_ui, Indent);
+
+            UpdateTooltip();
+            UpdateReadOnly();
+
+            return this;
+        }
+
+        protected override void UpdateLabel() {
+            if (!HasUI) return;
+
+            _ui.label.text = Translate(Label);
+        }
+
+        protected override void UpdateTooltip() {
+            if (!HasUI) return;
+
+            _ui.tooltip = IsInScope
+                ? string.IsNullOrEmpty(_tooltip)
+                    ? string.Empty // avoid invalidating UI if already no tooltip
+                    : Translate(_tooltip)
+                : Translate(INGAME_ONLY_SETTING);
+        }
+
+        protected override void UpdateReadOnly() {
+            if (!HasUI) return;
+
+            var readOnly = !IsInScope || _readOnly;
+
+            Log._Debug($"TriStateCheckboxOption.UpdateReadOnly() - `{FieldName}` is {(readOnly ? "read-only" : "writeable")}");
+
+            _ui.readOnly = readOnly;
+            _ui.opacity = readOnly ? 0.3f : 1f;
+        }
+
+        /* UI helper methods */
+
+        internal static void ApplyIndent(UIComponent component) {
+            UILabel label = component.Find<UILabel>("Label");
+
+            if (label != null) {
+                label.padding = new RectOffset(22, 0, 0, 0);
+            }
+
+            UISprite check = component.Find<UISprite>("Unchecked");
+
+            if (check != null) {
+                check.relativePosition += new Vector3(22.0f, 0);
+            }
+        }
+
+        internal static void ApplyTextWrap(UICheckBox checkBox, bool indented = false) {
+            UILabel label = checkBox.label;
+            bool requireTextWrap;
+            int maxWidth = indented ? LABEL_MAX_WIDTH_INDENTED : LABEL_MAX_WIDTH;
+            using (UIFontRenderer renderer = label.ObtainRenderer()) {
+                Vector2 size = renderer.MeasureString(label.text);
+                requireTextWrap = size.x > maxWidth;
+            }
+            label.autoSize = false;
+            label.wordWrap = true;
+            label.verticalAlignment = UIVerticalAlignment.Middle;
+            label.textAlignment = UIHorizontalAlignment.Left;
+            label.size = new Vector2(maxWidth, requireTextWrap ? 40 : 20);
+            if (requireTextWrap) {
+                checkBox.height = 42; // set new height + top/bottom 1px padding
+            }
+        }
+    }
+}
---
+++ TLM/TLM/UI/Helpers/UITriStateCheckbox.cs
@@ -0,0 +1,104 @@
+namespace TrafficManager.UI.Helpers {
+    using ColossalFramework.UI;
+    using KianCommons.UI;
+    using System;
+    using TrafficManager.U;
+    using TrafficManager.Util;
+    using UnityEngine;
+
+    public class UITriStateCheckbox : UICheckBox {
+        public event PropertyChangedEventHandler<bool?> eventValueChanged;
+
+        public UIComponent FalseComponent;
+        public UIComponent NullComponent;
+        public UIComponent TrueComponent {
+            get => checkedBoxObject;
+            set => checkedBoxObject = value;
+        }
+
+        private bool? value_;
+        public bool? Value {
+            get => value_;
+            set {
+                if (value != value_) {
+                    value_ = value;
+                    OnValueChanged();
+                }
+            }
+        }
+
+        [Obsolete("use Value instead", error: true)]
+        public new bool ?isChecked { get; set; }
+
+        protected virtual void OnValueChanged() {
+            RefreshIcon();
+            eventValueChanged?.Invoke(this, Value);
+        }
+
+        private void RefreshIcon() {
+            if (TrueComponent) TrueComponent.isVisible = Value == true;
+            if (FalseComponent) FalseComponent.isVisible = Value == false;
+            if (NullComponent) NullComponent.isVisible = Value == null;
+            RefreshTooltip();
+        }
+
+        public override void Awake() {
+            base.Awake();
+            name = GetType().Name;
+            size = new Vector2(716, 22);
+            IntVector2 spriteSize = new(120, 30);
+            AtlasBuilder atlasBuilder = new AtlasBuilder(name, "Options", spriteSize);
+            string[] names = new[] { "tristate-false", "tristate-null", "tristate-true" };
+            foreach(string name in names)
+                atlasBuilder.Add(new AtlasSpriteDef(name, spriteSize));
+            var atlas = atlasBuilder.CreateAtlas();
+
+            UISprite sprite = AddUIComponent<UISprite>();
+            sprite.atlas = atlas;
+            sprite.spriteName = names[0];
+            sprite.tooltip = "no";
+            sprite.size = new(88, 22); 
+            sprite.relativePosition = new Vector2(0, (height - sprite.height) / 2);
+            FalseComponent = sprite;
+
+            var sprite2 = AddUIComponent<UISprite>();
+            sprite2.atlas = atlas;
+            sprite2.spriteName = names[1];
+            sprite2.tooltip = "N/A";
+            sprite2.size = sprite.size;
+            sprite2.relativePosition = sprite.relativePosition;
+            NullComponent = sprite2;
+
+            var sprite3 = AddUIComponent<UISprite>();
+            sprite3.atlas = atlas;
+            sprite3.spriteName = names[2];
+            sprite3.tooltip = "yes";
+            sprite3.size = sprite.size;
+            sprite3.relativePosition = sprite.relativePosition;
+            TrueComponent = sprite3;
+
+            label = AddUIComponent<UILabel>();
+            label.text = name;
+            label.relativePosition = new Vector2(sprite.width + 5f, (height - label.height) / 2 + 1);
+        }
+
+        public override void Start() {
+            base.Start();
+            RefreshIcon();
+        }
+
+        protected override void OnClick(UIMouseEventParameter p) {
+            if (!readOnly && !p.used) {
+                Value = Value switch {
+                    true => false,
+                    false=> null,
+                    null => true,
+                };
+                p.Use();
+            }
+
+            base.OnClick(p);
+        }
+    }
+
+}
