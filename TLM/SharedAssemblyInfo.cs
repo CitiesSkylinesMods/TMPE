259ee42bcdf68e9e1d5d4d1323cadfd1fdc3b659
krzychu124 <krzychu124@gmail.com>
2025-09-23 20:00:24 +0200

    Compatibility patch for 1.20.1-f1

--- TLM/SharedAssemblyInfo.cs
+++ TLM/SharedAssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("11.9.1.*")]
+[assembly: AssemblyVersion("11.9.2.*")]
